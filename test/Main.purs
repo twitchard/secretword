628f59c65b15a473f965b89a8e3564fcc4d04f9a
Richard Marmorstein <richard.marmorstein@livestream.com>
2018-02-11 16:13:48 -0500

    Nice test function

--- test/Main.purs
+++ test/Main.purs
@@ -2,14 +2,20 @@ module Test.Main where
 
 import Prelude
 
+import Control.Monad.Aff (Aff)
+import Control.Monad.Aff.AVar (AVAR)
+import Control.Monad.Aff.Console (CONSOLE)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Random (RANDOM)
 import DB (class DB)
 import Data.Foreign (Foreign)
 import Data.Lens (_Just, view)
-import Data.Maybe (Maybe(..), isNothing)
-import Simple.JSON (write)
+import Data.Maybe (Maybe(Nothing, Just))
+import Simple.JSON (write, writeJSON)
 import Skill (handle, speeches)
 import Test.Unit (suite, test)
 import Test.Unit.Assert (assert) as Assert
+import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
 import Types (Session, textOf)
 import Web.Amazon.Alexa.Lens (_outputSpeech, _response, _sessionAttributes, _text, _type)
@@ -78,57 +84,85 @@ testEvent = {
   }
 }
 
+testHandle :: ∀ e.
+  Foreign →
+  TestDB →
+  String →
+  String →
+  Session →
+  (Aff ( random :: RANDOM , console :: CONSOLE | e)) Unit
+testHandle event db expectedType expectedText expectedSession = do
+  result <- handle db event dummy
+  let outputType = view (_response <<< _outputSpeech <<< _Just <<< _type) result
+      text = view (_response <<< _outputSpeech <<< _Just <<< _text ) result
+      session = view (_sessionAttributes ) result
+
+  Assert.assert
+    ("Speech should be type \"" <> expectedType <> "\" not \"" <> outputType <> "\".")
+    $ outputType == expectedType
+
+  Assert.assert
+    ("Alexa should say \"" <> expectedText <> "\", not \"" <> text <> "\".")
+    $ text == expectedText
+
+  Assert.assert
+    ("The session should be \"" <> writeJSON expectedSession <> "\", not \"" <> writeJSON session <> "\".")
+    $ eqSessions expectedSession session
+
+  where
+    eqSessions Nothing Nothing = true
+    eqSessions (Just s) (Just s') =
+      s.secretWord == s'.secretWord
+      && s.guesses == s'.guesses
+      && s.status == s'.status
+    eqSessions _ _ = false
+
+main :: forall t255.
+   Eff
+     ( console :: CONSOLE
+     , testOutput :: TESTOUTPUT
+     , avar :: AVAR
+     , random :: RANDOM
+     | t255
+     )
+     Unit
 main = runTest do
   suite "End-to-end" do
     test "Handling a guess of the correct answer" do
-      let db = emptyDB
-          event = write testEvent
-          context = dummy
+      let event = write testEvent
+          db = emptyDB
+          expectedType = "PlainText"
+          expectedText = textOf $ speeches.youWin 1
+          expectedSession = Nothing
 
-      result <- handle db event context
-
-      let speechType = view (_response <<< _outputSpeech <<< _Just <<< _type) result
-          speechText = view (_response <<< _outputSpeech <<< _Just <<< _text ) result
-          session = view (_sessionAttributes ) result
-
-      Assert.assert
-        (speechType <> " should be PlainText")
-        $ speechType == "PlainText"
-
-      Assert.assert
-        ("Alexa should say \"" <> (textOf $ speeches.youWin 1) <> "\", not \"" <> speechText<> "\"")
-        $ speechText == (textOf $ speeches.youWin 1)
-
-      Assert.assert
-        ("The session should end")
-        $ isNothing session
+      testHandle event db expectedType expectedText expectedSession
 
     test "Handling a malformed AlexaRequest" do
-      let db = emptyDB
-          event = write
+      let event = write
                     (testEvent
                       { request = testEvent.request
                                     { type = "MalformedRequest"
                                     }
                       }
                     )
-          context = dummy
 
-      result <- handle db event context
+          db = emptyDB
+          expectedType = "PlainText"
+          expectedText = textOf $ speeches.couldntUnderstand
+          expectedSession = Nothing
+      testHandle event db expectedType expectedText expectedSession
 
-      let speechType = view (_response <<< _outputSpeech <<< _Just <<< _type) result
-          speechText = view (_response <<< _outputSpeech <<< _Just <<< _text ) result
-          session = view (_sessionAttributes ) result
+    test "Handling a malformed Guess" do
+      let event = write
+                    (testEvent
+                      { request = testEvent.request
+                                    { type = "MalformedRequest"
+                                    }
+                      }
+                    )
 
-      Assert.assert
-        (speechType <> " should be PlainText")
-        $ speechType == "PlainText"
-
-      Assert.assert
-        ("Alexa should say \"" <> (textOf speeches.couldntUnderstand) <> "\", not \"" <> speechText <> "\"")
-        $ speechText == (textOf speeches.couldntUnderstand)
-
-      Assert.assert
-        ("The session should end")
-        $ isNothing session
-
+          db = emptyDB
+          expectedType = "PlainText"
+          expectedText = textOf $ speeches.couldntUnderstand
+          expectedSession = Nothing
+      testHandle event db expectedType expectedText expectedSession
